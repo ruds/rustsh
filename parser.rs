ea07cfc8b1b84d797788dda940c310b678ea1969
Matt Rudary <matt@rudary.com>
2012-03-18 21:39:12 -0400

    Finish up the parser.

--- parser.rs
+++ parser.rs
@@ -34,11 +34,12 @@ type command = {args: [str],
                 error: output_sink };
 
 enum command_line {
+    singleton(command),
     pipeline([command_line]),
     sequence([command_line]),
     background(@command_line),
-    and(@command_line, @command_line),
-    or(@command_line, @command_line),
+    and([command_line]),
+    or([command_line]),
 }
 
 enum parse_result {
@@ -109,9 +110,98 @@ enum part_parse {
     sep(token),
 }
 
+fn part_to_cl(p: part_parse) -> command_line {
+    ret alt p {
+      cmd(c) { singleton(c) }
+      subshell(cl) { cl }
+      sep(_) { fail("part_to_cl doesn't convert separators."); }
+    };
+}
+
+fn append_to_cl(&cl: command_line, p: part_parse) {
+    cl = alt cl {
+      singleton(_) { fail("cl may not be singleton."); }
+      pipeline(args) { pipeline(args + [part_to_cl(p)]) }
+      sequence(args) { sequence(args + [part_to_cl(p)]) }
+      background(_) { sequence([cl, part_to_cl(p)]) }
+      and(args) { and(args + [part_to_cl(p)]) }
+      or(args) { or(args + [part_to_cl(p)]) }
+    };
+}
+
 fn finish_parse(parts: [part_parse]) -> parse_result {
     assert vec::is_not_empty(parts);
-    fail("not implemented.");
+    let cur_cl = alt parts[0u] {
+      sep(_)  { ret error("No initial command."); }
+      _ { part_to_cl(parts[0u]) }
+    };
+    if vec::len(parts) == 1u {
+        ret parsed(cur_cl);
+    }
+    let idx = 1u;
+    let cmd_required = false;
+    let cmd_allowed = false;
+    while idx < vec::len(parts) {
+        alt parts[idx] {
+          cmd(_)
+          | subshell(_) {
+            if !cmd_allowed {
+                ret error("Found a command where a separator was expected.");
+            }
+            cmd_required = false;
+            cmd_allowed = false;
+            alt cur_cl {
+              _ { append_to_cl(cur_cl, parts[idx]); }
+            }
+          }
+          sep(t) {
+            if cmd_required {
+                ret error("Found a separator where a command was expected.");
+            }
+            cmd_required = true;
+            cmd_allowed = true;
+            cur_cl = alt t {
+              tokenizer::pipe {
+                alt cur_cl {
+                  pipeline(_) { cur_cl }
+                  _ { pipeline([cur_cl]) }
+                }
+              }
+              tokenizer::and {
+                alt cur_cl {
+                  and(_) { cur_cl }
+                  _ { and([cur_cl]) }
+                }
+              }
+              tokenizer::or {
+                alt cur_cl {
+                  or(_) { cur_cl }
+                  _ { or([cur_cl]) }
+                }
+              }
+              tokenizer::background {
+                cmd_required = false;
+                background(@cur_cl)
+              }
+              tokenizer::sequence {
+                alt cur_cl {
+                  sequence(_) { cur_cl }
+                  _ { sequence([cur_cl]) }
+                }
+              }
+              _ {
+                fail("Unexpected partial parse.");
+              }
+            }
+          }
+        }
+        idx += 1u;
+    }
+    ret if cmd_required {
+        error("Missing command at end of line.")
+    } else {
+        parsed(cur_cl)
+    };
 }
 
 fn parse_tokens(tokens: [token], level: uint, &idx: uint) -> parse_result {
@@ -203,5 +293,66 @@ fn test_make_command() {
                       tokenizer::background]) {
       left(_) { assert false; }
       right(_) { assert true; }
+    }
+}
+
+#[test]
+fn simple_cmdline() {
+    assert parse(tokenizer::tokenize("  hi there"))
+        == parsed(singleton({args: ["hi", "there"],
+                             input: stdin,
+                             output: stdout,
+                             error: stderr}));
+}
+
+#[test]
+fn complex_pipeline() {
+    assert parse(tokenizer::tokenize("(cat abc d\"e f\\\"\"g; echo 'hello\\') |"
+                                     + " grep -i he >matches &"))
+        == parsed(background(@pipeline(
+            [sequence([singleton({args: ["cat", "abc", "de f\"g"],
+                                  input: stdin,
+                                  output: stdout,
+                                  error: stderr}),
+                       singleton({args: ["echo", "hello\\"],
+                                  input: stdin,
+                                  output: stdout,
+                                  error: stderr})]),
+             singleton({args: ["grep", "-i", "he"],
+                        input: stdin,
+                        output: outfile("matches"),
+                        error: stderr})])));
+}
+
+#[test]
+fn test_continuation() {
+    assert parse(tokenizer::tokenize("foo && bar && \\"))
+        == continuation_required;
+}
+
+#[test]
+fn test_binary_operators() {
+    alt parse(tokenizer::tokenize("foo && | bar")) {
+      error(_) { assert true; }
+      _ { assert false; }
+    }
+
+    assert parse(tokenizer::tokenize("foo && bar && baz"))
+        == parsed(and([singleton({args: ["foo"],
+                                  input: stdin,
+                                  output: stdout,
+                                  error: stderr}),
+                       singleton({args: ["bar"],
+                                  input: stdin,
+                                  output: stdout,
+                                  error: stderr}),
+                       singleton({args: ["baz"],
+                                  input: stdin,
+                                  output: stdout,
+                                  error: stderr})]));
+
+    alt parse(tokenizer::tokenize("foo && bar &&")) {
+      error(_) { assert true; }
+      _ { assert false; }
     }
 }
